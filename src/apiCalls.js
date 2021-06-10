0b6fa128d69bdb7fb855a9e1bbf42f968316e139
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-06-09 20:58:41 -0400

    Create apicalls

---
+++ src/apiCalls.js
@@ -0,0 +1,43 @@
+const fetchSingleCustomer = (userId) => {
+    return fetch(`http://localhost:3001/api/v1/customers/${userId}`)
+    .then(response => response.json())
+    .catch(err => console.error("not working"))
+}
+
+const fetchAllCustomers = () => {
+    return fetch('http://localhost:3001/api/v1/customers')
+      .then(response => response.json())
+      .catch(err => console.error("not working"))
+  }
+  const fetchRooms = () => {
+    return fetch('http://localhost:3001/api/v1/rooms')
+      .then(response => response.json())
+      .catch(err => console.error("not working"))
+  }
+  const fetchBookings = () => {
+    return fetch('http://localhost:3001/api/v1/bookings')
+      .then(response => response.json())
+      .catch(err => console.error("not working"))
+  }
+
+  export const getData = () => {
+    return Promise.all([fetchAllCustomers(), fetchSingleCustomer(), fetchRooms(), fetchBookings()])
+  }
+
+
+//   ERROR HANDLING
+
+export const displayError = (errorMessage) => {
+    const error =  document.getElementById('error');
+    const message = errorMessage.message === 'Failed to fetch' ?
+      "Internet connection may be unstable. Check again in a moment please." : errorMessage
+      error.innerText = message;
+}
+
+export const checkForError = response => {
+    if (!response.ok) {
+        throw new Error('Please fill out all required fields.');
+    } else {
+        return response.json();
+    }
+}
