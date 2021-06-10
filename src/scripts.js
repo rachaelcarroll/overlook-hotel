5ddc185c7f70da2420e6a55007bb0d58452ed726
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-06-10 00:36:31 -0400

    Style main page

--- src/scripts.js
+++ src/scripts.js
@@ -8,6 +8,7 @@ import apiCalls from './apiCalls'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/main-photo.jpg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
 let fetchCustomerData, fetchRoomsData, fetchBookingsData
